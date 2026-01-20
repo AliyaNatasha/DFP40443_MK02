0257806fe7074bd6b7ad01e3373341c8c33584f2
AliyaNatasha <natashaaliya235@gmail.com>
2026-01-20 17:13:54 +0800

    pengasingan kod

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$nama = "ALIYA NATASHA BINTI MOHAMAD BASRI";
+$nomatrik = "18DIT24F1010";
+$kelas = "DIT4E";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Saya</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Profil Diri</h1>
+    <img src="img/gmbr matrik.jpg" width="200" height="200" alt="profile gmbr matrik">
+    <p>Nama: <?=$nama?></p>
+    <p>No.Matrik: <?=$nomatrik?></p>
+    <p>Kelas: <?=$kelas?></p>
+</body>
+</html>
